c96be4af7cbc2487c74f3419f7e98dd8be8ffefe
kolyamoloko <kolyamoloko@mail.ru>
2021-11-07 21:26:39 +0300

    add: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const fs = require('fs');
+const folder = './04-copy-directory/files';
+const newFolder = './04-copy-directory/files-copy';
 
+fs.mkdir(`${newFolder}`, { recursive: true }, () => {});
+
+fs.readdir(folder, (err, files) => {
+    if (err){
+        throw err;
+    }
+  files.forEach(file => {
+    fs.copyFile(folder + '/' + file, newFolder + '/' + file, function () {});
+  });
+  console.log('files copy');
+});
+
+fs.readdir(newFolder, (err, files) => {
+    if (err){
+        throw err;
+    }
+  files.forEach(file => {
+
+    fs.stat(folder + '/' + file, function ( err) {
+      if (err) {
+        fs.unlink(newFolder + '/' + file, function () {});
+      }
+    });
+  });
+});
